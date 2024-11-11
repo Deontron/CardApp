4b99d3479ad072ae2aea4f6c2537f97628856d04
Ahmet Çelik <ahmet.celik2605@outlook.com>
2024-11-11 13:54:47 +0300

    cards

---
+++ CardApp/Assets/Scripts/Card.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public enum CardTypes
+    {
+        Club,
+        Diamond,
+        Heart,
+        Spades
+    }
+
+    public int CardId;
+    public CardTypes CardType;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
